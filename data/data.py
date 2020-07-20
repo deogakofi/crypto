fdef3edd74112d8fc8a139d01c6eb88e077f65e9
Deoga Kofi <deogakofi@gmail.com>
2020-07-20 15:39:54 +0100

    readme update

--- data/data.py
+++ data/data.py
@@ -6,7 +6,6 @@ from plotly.graph_objs import *
 import datetime
 from datetime import date, timedelta
 from datetime import datetime as dt
-import plotly.plotly as py
 from sklearn.preprocessing import MinMaxScaler
 import talib as ta
 from sklearn.preprocessing import MinMaxScaler
@@ -17,10 +16,32 @@ cg = CoinGeckoAPI()
 
 #Get bitcoin prices
 def get_btc_price(ids, vs_currencies):
-    return cg.get_price(ids='{}'.format(ids), vs_currencies='{}'.format(vs_currncies))
+    '''
+    This method gets the current price of any crypto currency
+    INPUT:
+    ids - (str) the id of the cryptocurrency
+    vs_currencies - (str) the currency format to return the price
+
+    OUTPUT:
+    price - (float) price of coin
+
+    '''
+    price = cg.get_price(ids='{}'.format(ids), vs_currencies='{}'.format(vs_currncies))
+    return price
 
 #Get Top 20 crypto coins in order of market_cap
 def get_top_20_mc(order, vs_currency, per_page, page):
+    '''
+    This method gets the top 20 cryptocurrency according to market cap
+    INPUT:
+    order - (str) the order of the list returned
+    vs_currencies - (str) the currency format to return the price
+    per_page  - (str) the number of results per page
+    page - (str) the page number you want to view
+    OUTPUT:
+    top_20_mc - (dataframe) top_20_mc coins in order of marketcap desc
+
+    '''
     top_20_mc = cg.get_coins_markets(order = '{}'.format(order),
     vs_currency='{}'.format(vs_currency), per_page = '{}'.format(per_page),
     page = '{}'.format(page))
@@ -36,6 +57,16 @@ origin_btc = pd.to_datetime('03/01/2009').date()
 
 #Get historic daily bitcoin close prices
 def get_historic_btc_price(id, vs_currency):
+    '''
+    This method gets the historic bitcoin price from the date of origin.
+    Can be used for other coins
+    INPUT:
+    id - (str) the id of the cryptocurrency. in this case 'bitcoin'
+    vs_currencies - (str) the currency format to return the price
+    OUTPUT:
+    historic_btc_price - (dataframe) contains historic bitcoin price and tidy data
+
+    '''
 
     origin_btc = pd.to_datetime('03/01/2009').date()
     n_days = (today -origin_btc).days
@@ -77,12 +108,25 @@ def get_historic_btc_price(id, vs_currency):
     historic_btc_price['month'] = historic_btc_price.date.dt.month
     historic_btc_price['year'] = historic_btc_price.date.dt.year
     historic_btc_price['date_clean'] = historic_btc_price.date.dt.date
+    #prepare data for analysis by setting the date column to datetime index
+    historic_btc_price['date_clean'] = pd.to_datetime(historic_btc_price.date_clean)
+    historic_btc_price = historic_btc_price.set_index('date_clean')
+    historic_btc_price['day_of_week'] = historic_btc_price.date.dt.day_name()
 
     return historic_btc_price
+#Initialize historic_btc_price for future use
 historic_btc_price = get_historic_btc_price('bitcoin', 'gbp')
 
 #plot Top 20 crypto coins in order of market_cap
 def plot_top_20_mc():
+    '''
+    This method plots the top 20 cryptocurrencies in order of market cap
+    INPUT:
+    None
+    OUTPUT:
+    Plot of the top 20 cryptocurrencies in order of market cap
+    '''
+
     top_mc = top_20_mc[['id', 'symbol', 'current_price', 'market_cap']].sort_values(by = 'market_cap',
     ascending = False)
     top_mc['market_cap'] = (top_mc.market_cap)/1000000000
@@ -95,6 +139,13 @@ def plot_top_20_mc():
 
 #plot Top 20 crypto coins in order of volume
 def plot_top_20_volume():
+    '''
+    This method plots the top 20 cryptocurrencies in order of volume
+    INPUT:
+    None
+    OUTPUT:
+    Plot of the top 20 cryptocurrencies in order of volume
+    '''
     top_volume = top_20_mc[['id', 'symbol', 'total_volume']].sort_values(by = 'total_volume',
     ascending = False)
     top_volume['total_volume'] = (top_volume.total_volume)/1000000
@@ -107,6 +158,14 @@ def plot_top_20_volume():
 
 #plot historic bitcoin price
 def plot_historic_btc_price():
+    '''
+    This method plots the historic bitcoin price
+    INPUT:
+    None
+    OUTPUT:
+    Plot of the historic bitcoin price
+    '''
+
     plt.plot(historic_btc_price.date_clean, historic_btc_price.price.astype(float))
     plt.xticks(rotation=90)
     plt.ylabel('BTC Price (£m)')
@@ -116,6 +175,15 @@ def plot_historic_btc_price():
 
 #Get 90-Day bitcoin price candlesticks with minutely data
 def get_btc_candlesticks(id, vs_currency):
+    '''
+    This method gets the 90 day OHLC data for bitcoin
+    INPUT:
+    id - (str) the id of the cryptocurrency. in this case 'bitcoin'
+    vs_currencies - (str) the currency format to return the price
+    OUTPUT:
+    price_ohlc_reindex - (dataframe) contains 90 day OHLC data for bitcoin
+    '''
+
     n = 90
     date_N_days_ago = dt.now() - timedelta(days=n)
 
@@ -156,7 +224,6 @@ def get_btc_candlesticks(id, vs_currency):
     #concat price, market_caps and total_volume
     bitcoin_price_date_N_days_ago = pd.concat([prices_ohlc, market_caps_ohlc, total_volume_ohlc], axis = 1)
 
-
     bitcoin_price_date_N_days_ago['timestamp'] = bitcoin_price_date_N_days_ago.timestamp.astype(int)
     bitcoin_price_date_N_days_ago['date'] = pd.to_datetime(bitcoin_price_date_N_days_ago.timestamp, unit='s')
     bitcoin_price_date_N_days_ago['day'] = bitcoin_price_date_N_days_ago.date.dt.day
@@ -170,15 +237,21 @@ def get_btc_candlesticks(id, vs_currency):
     price_ohlc_reindex = bitcoin_price_date_N_days_ago.set_index('date')['price'].resample('D').ohlc()
     return price_ohlc_reindex
 
-#prepare data for analysis by setting the date column to datetime index
-historic_btc_price['date_clean'] = pd.to_datetime(historic_btc_price.date_clean)
-historic_btc_price = historic_btc_price.set_index('date_clean')
-historic_btc_price['day_of_week'] = historic_btc_price.date.dt.day_name()
-
-print(historic_btc_price.head())
-
 #Perform SMA analysis to determine buy and sell signals
+
 def SMA(close,sPeriod,lPeriod):
+    '''
+    This creates the simple moving average data for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    smaSell - categorical series indicating when to sell
+    smaBuy - categorical series indicating when to buy
+    shortSMA - Series containing the shortSMA calculations
+    longSMA - Series containing the longSMA calculations
+    '''
     shortSMA = ta.SMA(close,sPeriod)
     longSMA = ta.SMA(close,lPeriod)
     smaSell = ((shortSMA <= longSMA) & (shortSMA.shift(1) >= longSMA.shift(1)))
@@ -186,6 +259,15 @@ def SMA(close,sPeriod,lPeriod):
     return smaSell,smaBuy,shortSMA,longSMA
 
 def SMA_signals(close, sPeriod, lPeriod):
+    '''
+    This creates the simple moving average signals for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    signals_SMA - (dataframe) merged buy and sell signal from SMA analysis
+    '''
     #tidy SMA data to plot
     smaSell,smaBuy,shortSMA,longSMA = SMA(close, sPeriod, lPeriod)
     signals_SMA = pd.concat([smaSell, smaBuy, shortSMA, longSMA, close], axis = 1, keys = ['smaSell', 'smaBuy', 'shortSMA', 'longSMA', 'close'])
@@ -201,9 +283,17 @@ def SMA_signals(close, sPeriod, lPeriod):
     signals_SMA['day_of_week'] = signals_SMA.date.dt.day_name()
     signals_SMA.drop('date', axis = 1, inplace = True)
     return signals_SMA
-print('----------------------------------------------')
-print(SMA_signals(historic_btc_price.price, 14,49))
+
 def plot_SMA(close, sPeriod, lPeriod):
+    '''
+    This creates the simple moving average plot for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    plot showing the simple moving average for historic bitcoin data
+    '''
     signals_SMA = SMA_signals(close, sPeriod, lPeriod)
     # Plot the closing price and visualise signals for SMA
     plt.plot(historic_btc_price['price'], lw=2., color = 'purple')
@@ -221,6 +311,16 @@ def plot_SMA(close, sPeriod, lPeriod):
     plt.show()
 
 def create_SMA_portfolio(close, sPeriod, lPeriod):
+    '''
+    This creates the simple moving average portfolio to calculate returns
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    portfolio - (dataframe) simulation of a portfolio that buys and sells according
+    to SMA signals
+    '''
     signals_SMA = SMA_signals(close, sPeriod, lPeriod)
     # Create portfolio and Set the initial capital SMA
     initial_capital= float(150000.0)
@@ -251,6 +351,18 @@ def create_SMA_portfolio(close, sPeriod, lPeriod):
 
 origin = pd.to_datetime('2013-04-28')
 def calculate_SMA_pct_change(close, sPeriod, lPeriod):
+    '''
+    This calculates the returns from investing in the SMA buy and sell signals
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    avg_pct_change - (int) the avg yearly percentage returns according
+    to SMA signals
+    total_pct_change - (int) the total percentage returns according
+    to SMA signals
+    '''
     portfolio = create_SMA_portfolio(close, sPeriod, lPeriod)
     avg_pct_change = ((((portfolio.loc[today]['total']).drop_duplicates() - (portfolio.loc[origin]['total']))/portfolio.loc[origin]['total'])/(portfolio.year.max() - portfolio.year.min()))*100
     total_pct_change = ((((portfolio.loc[today]['total']).drop_duplicates() - (portfolio.loc[origin]['total']))/portfolio.loc[origin]['total']))*100
@@ -260,6 +372,15 @@ def calculate_SMA_pct_change(close, sPeriod, lPeriod):
     return avg_pct_change, total_pct_change
 
 def plot_SMA_returns(close, sPeriod, lPeriod):
+    '''
+    This calculates the returns from investing in the SMA buy and sell signals
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    sPeriod - (int)  the short SMA period
+    lPeriod - (int)  the long SMA period
+    OUTPUT:
+    Scaled plot showing the SMA portfolio returns compared with the price of bitcoin
+    '''
     portfolio = create_SMA_portfolio(close, sPeriod, lPeriod)
     scaler = MinMaxScaler()
     plt.plot(scaler.fit_transform(historic_btc_price['price'].values.reshape(-1, 1)), lw=2., color = 'purple')
@@ -269,6 +390,16 @@ def plot_SMA_returns(close, sPeriod, lPeriod):
 
 #perform RSI analysis
 def RSI(close,timePeriod):
+    '''
+    This creates the relative strength index data for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    rsiSell - categorical series indicating when to sell
+    rsiBuy - categorical series indicating when to buy
+    rsi - Series containing the rsi calculations
+    '''
     rsi = ta.RSI(close,timePeriod)
     rsiSell = (rsi>80) & (rsi.shift(1)<=80)
     rsiBuy = (rsi<20) & (rsi.shift(1)>=20)
@@ -276,6 +407,14 @@ def RSI(close,timePeriod):
 
 
 def RSI_signals(close, timePeriod):
+    '''
+    This creates the RSI signals for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    signals_RSI - (dataframe) merged buy and sell signal from RSI analysis
+    '''
     rsiSell,rsiBuy, rsi = RSI(close, timePeriod)
     #tidy RSI data to plot
     signals_RSI = pd.concat([rsiSell,rsiBuy, rsi, close], axis = 1, keys = ['rsiSell','rsiBuy', 'rsi', 'close'])
@@ -288,6 +427,14 @@ def RSI_signals(close, timePeriod):
 
 
 def plot_RSI(close, timePeriod):
+    '''
+    This creates the RSI plot for historic bitcoin data
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    plot showing the RSI for historic bitcoin data
+    '''
     signals_RSI = RSI_signals(close, timePeriod)
     # Plot the closing price and visualise signals for RSI
     plt.plot(historic_btc_price['price'], lw=2., color = 'purple')
@@ -306,6 +453,15 @@ def plot_RSI(close, timePeriod):
 
 
 def create_RSI_portfolio(close, timePeriod):
+    '''
+    This creates the relative strength index portfolio to calculate returns
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    portfolio - (dataframe) simulation of a portfolio that buys and sells according
+    to RSI signals
+    '''
     signals_RSI = RSI_signals(close, timePeriod)
     # Create portfolio and Set the initial capital RSI
     initial_capital= float(150000.0)
@@ -334,6 +490,17 @@ def create_RSI_portfolio(close, timePeriod):
     return portfolio
 
 def calculate_RSI_pct_change(close, timePeriod):
+    '''
+    This calculates the returns from investing in the SMA buy and sell signals
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    avg_pct_change - (int) the avg yearly percentage returns according
+    to RSI signals
+    total_pct_change - (int) the total percentage returns according
+    to RSI signals
+    '''
     portfolio = create_RSI_portfolio(close, timePeriod)
     avg_pct_change = ((((portfolio.loc[today]['total']).drop_duplicates() - (portfolio.loc[origin]['total']))/portfolio.loc[origin]['total'])/(portfolio.year.max() - portfolio.year.min()))*100
     total_pct_change = ((((portfolio.loc[today]['total']).drop_duplicates() - (portfolio.loc[origin]['total']))/portfolio.loc[origin]['total']))*100
@@ -344,6 +511,14 @@ def calculate_RSI_pct_change(close, timePeriod):
 
 
 def plot_RSI_returns(close, timePeriod):
+    '''
+    This calculates the returns from investing in the SMA buy and sell signals
+    INPUT:
+    close - (series) the daily close price of bitcoin
+    timePeriod - (int)  timePeriod to base RSI calculations on
+    OUTPUT:
+    Scaled plot showing the RSI portfolio returns compared with the price of bitcoin
+    '''
     portfolio = create_RSI_portfolio(close, timePeriod)
     scaler = MinMaxScaler()
     plt.plot(scaler.fit_transform(historic_btc_price['price'].values.reshape(-1, 1)), lw=2., color = 'purple')
@@ -354,7 +529,14 @@ def plot_RSI_returns(close, timePeriod):
 
 
 def return_figures():
-
+    '''
+    This returns all the figures for the flask app
+    INPUT:
+    None
+    OUTPUT:
+    Plots for flask app
+    '''
+    #Create Graph one
     graph_one = []
     df_one = get_top_20_mc('market_cap_desc',
     'gbp', 20, 1).sort_values(by = 'market_cap', ascending = False)
@@ -379,7 +561,7 @@ def return_figures():
                       autotick=False),
                     yaxis = dict(title = 'Marketcap (£Bn)'),
                     )
-
+    #Create Graph two
     graph_two = []
     df_two = df_one.sort_values(by = 'total_volume', ascending = False)
     coin_list = df_two.id.tolist()
@@ -401,6 +583,7 @@ def return_figures():
                   yaxis = dict(title = 'Volume (£Bn)'),
                   )
 
+    #Create Graph three
     graph_three = []
     df_three = historic_btc_price.groupby('year').mean()['price'].sort_values()
     year_list = df_three.index.tolist()
@@ -423,6 +606,7 @@ def return_figures():
               yaxis = dict(title = 'Average yearly bitcoin price (£)'),
               )
 
+    #Create Graph four
     graph_four = []
     df_four = historic_btc_price.groupby('month').mean()['price'].sort_index()
     df_four.index = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
@@ -449,6 +633,8 @@ def return_figures():
               )
 
 
+
+    #Create Graph five
     graph_five = []
     df_five = historic_btc_price.groupby('day_of_week').mean()['price'].sort_values()
     day_of_week = df_five.index.tolist()
@@ -471,7 +657,7 @@ def return_figures():
               yaxis = dict(title = 'Average price (£)'),
               )
 
-
+    #Create Graph five
     sma_range = 50
     log_yaxis = False
     df_six = get_btc_candlesticks('bitcoin', 'gbp')
@@ -493,7 +679,7 @@ def return_figures():
             x = df_six.index,
             y = df_six.close.rolling(i).mean(), # Pandas SMA
             name = "SMA" + str(i),
-            mode = "line",
+            mode = "lines",
             line = dict(color = "green"),
             opacity = 0.7,
             visible = False,
@@ -644,6 +830,7 @@ def return_figures():
 
     if log_yaxis: layout["yaxis"]["type"] = "log"
 
+    #Create Graph seven
     graph_seven = []
     df_seven = historic_btc_price
     df_seven_signals = SMA_signals(df_seven.price, 21, 49)
@@ -655,7 +842,7 @@ def return_figures():
         visible=True,
         line={'color': 'blue'},
         name='btc',
-        mode='line',
+        mode='lines',
         opacity = 0.7)
     trace_two = Scatter(x = df_seven.index.to_list(),
         y=df_seven_signals.shortSMA.to_list(),
@@ -663,14 +850,14 @@ def return_figures():
         line={'color': 'green'},
         name='smaShort(21)',
         opacity = 0.7,
-        mode='line')
+        mode='lines')
     trace_three = Scatter(x = df_seven.index.to_list(),
         y=df_seven_signals.longSMA.to_list(),
         visible=True,
         line={'color': 'red'},
         name='smaLong(49)',
         opacity = 0.7,
-        mode='line')
+        mode='lines')
     trace_four = Scatter(
         name = 'buy',
         mode = 'markers',
@@ -763,7 +950,7 @@ def return_figures():
 
     if log_yaxis: layout["yaxis"]["type"] = "log"
 
-
+    #Create Graph eight
     graph_eight = []
     df_eight = historic_btc_price
     df_eight_signals = RSI_signals(df_eight.price, 14)
@@ -774,7 +961,7 @@ def return_figures():
         visible=True,
         line={'color': 'blue'},
         name='btc',
-        mode='line',
+        mode='lines',
         opacity = 0.7)
     trace_two = Scatter(
         name = 'buy',
@@ -869,6 +1056,7 @@ def return_figures():
 
     if log_yaxis: layout["yaxis"]["type"] = "log"
 
+    #Create Graph Nine
     avg_yearly_gains_SMA, total_gains_SMA = calculate_SMA_pct_change(historic_btc_price.price, 21,49)
     avg_yearly_gains_RSI, total_gains_RSI = calculate_RSI_pct_change(historic_btc_price.price, 14)
     graph_nine = Figure(data=[Table(header=dict(values=['Model', 'avg_yearly_gains_pct', 'total_gains_pct']),
@@ -890,5 +1078,3 @@ def return_figures():
     figures.append(dict(data=graph_eight, layout=layout_eight))
     figures.append(dict(data=graph_nine))
     return figures
-
-plotly.offline.plot(return_figures()[3])
--- readme.md
+++ readme.md
@@ -1,7 +1,35 @@
-WORLDBANK DATA DASHBOARD
+CRYPTO DASHBOARD
 -----------------------
-Create a dashboard to visualise WorldBank data for easy analysis.
-App should run on browser displaying dashboard
+Motivation
+------------
+Having previously dabbled in the trading of cryptocurrency in 2016 before the huge boom and bust, I have always wondered if there was a less risky way to reap the benefits of trading and if there was a way to know when to buy or sell.
+
+If there was a system to do this then everyone would be doing it and be rich by now but the reality is, it is possible to build a system to buy and sell crypto but the performance will depend on the quality of data and methodology chosen.
+
+Approach
+-----------
+There are a few approaches that can be taken to build the system.
+* Sentiment analysis - Scraping for sentiments on the coin and analysing to make trading decisions
+* Technical analysis - Using the historical price of the coin to make trading decisions
+* A combination of both
+
+I decided to go for the latter for proof of concept as there is more readily available data for free to explore this method.
+
+Techniques
+-------------
+During technical analysis the following systems were used to make trading decisions:
+* SMA - Using simple moving averages to determine when to buy or sell based on the intercept of the short and long moving averages.
+
+* RSI - Performing relative strength analysis on a 14 day cycle and using a threshold of 80%
+
+Traditional machine learning was not used for this project because it doesn't add any real life value to trading as it simply applies a smoothing curve with no logic.
+
+Performance of Models
+-------------------------
+* SMA - Average year gains = 0.6966840372094414%
+        Total gains = 4.87678826046609%
+* RSI - Average year gains = 3.1764317126603365%
+        Total gains = 22.235021988622357%
 
 
 Related Blog
@@ -15,12 +43,37 @@ Installation
 
 * Clone this repo to your computer.
 * `myapp.py` is the executable for the app
+
 * `data` folder contains three datasets
-    * `data.py`: Extracts data from worldbank API using country codes and indicators. Instructions on how to use the API can be found on https://datahelpdesk.worldbank.org/knowledgebase/articles/898581-api-basic-call-structures
-      * `data.py` contains a `return_figures` method that returns the json figures to plot in graphs
+    * `data.py`: Extracts data from coingecko API.
+    Instructions on how to use the API can be found on https://www.coingecko.com/en/api
+      * `data.py` contains
+      * `get_btc_price`: This method returns the current price of any crypto currency
+      * `get_top_20_mc`: This method returns the top 20 cryptocurrency according to market cap in a dataframe
+      * `get_historic_btc_price`: This method returns the historic bitcoin price from the date of origin. Can be used for other coins
+      * `plot_top_20_mc`: This method plots the top 20 cryptocurrencies in order of market cap
+      * `plot_top_20_volume`: This method plots the top 20 cryptocurrencies in order of volume
+      * `plot_historic_btc_price`: This method plots the historic bitcoin price
+      * `get_btc_candlesticks`: This method gets the 90 day OHLC data for bitcoin
+      * `SMA`: This creates the simple moving average data for historic bitcoin data
+      * `SMA_signals`: This creates the simple moving average signals for historic bitcoin data
+      * `plot_SMA`: This creates the simple moving average plot for historic bitcoin data
+      * `create_SMA_portfolio`: This creates the simple moving average portfolio to calculate returns
+      * `calculate_SMA_pct_change`: This calculates the returns from investing in the SMA buy and sell signals
+      * `plot_SMA_returns`: This calculates the returns from investing in the SMA buy and sell signals
+      * `RSI`: This creates the relative strength index data for historic bitcoin data
+      * `RSI_signals`: This creates the relative strength index signals for historic bitcoin data
+      * `plot_RSI`: This creates the relative strength index plot for historic bitcoin data
+      * `create_RSI_portfolio`: This creates the relative strength index portfolio to calculate returns
+      * `calculate_RSI_pct_change`: This calculates the returns from investing in the RSI buy and sell signals
+      * `plot_RSI_returns`: This calculates the returns from investing in the RSI buy and sell signals
+      * `return_figures`: method that returns the json figures to plot in graphs
+
   * `myapp` folder contains
     * `static`: Folder containing all image files
-    * `templates`: Folder containing `index.html` homepage which renders when you run the app. It was designed with bootstrap
+    * `templates`: Folder containing
+      * `index.html` homepage which renders when you run the app. It was designed with bootstrap
+      * `trade_view.html` tradeview for bitcoin which renders when you run the app
     * `__init__.py`: Initiate flask app
     * `routes.py`: Defines the app routes
 * It is recommended you run the solution in a virtual environment. Please see https://docs.python.org/3/library/venv.html
@@ -43,9 +96,9 @@ Extending this
 
 If you want to extend this work, here are a few places to start:
 
-* Include more indicators in the  dataset
+* Get access to API with more detailed data preferably shorter ticks
 * Include more graphs
-* Improve HTML layout and introduce filters to graphs using ginger
+* Improve buy and sell signals and explore more methods
 
 
 
